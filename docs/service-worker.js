d33b717cf4e674f1dfc38e6d256fff8077d836d5
Maliki <39995350+AkbarMaliki@users.noreply.github.com>
2020-08-10 22:38:53 +0800

    pro

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.42f0c23885401028dd4649105711d806.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.d517c5247fcf7348233384914733ae12.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 if (workbox) {
   console.log(`Workbox is loaded`);
