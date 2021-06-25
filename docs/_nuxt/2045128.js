db163bab4a80396c693d57d35b2e9d36e2cacc2e
AkbarMaliki <taufikakbarmalikitkj@gmail.com>
2021-06-25 22:13:22 +0800

    wokey

---
+++ docs/_nuxt/2045128.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{445:function(n,w,o){"use strict";o.r(w);o(259)}}]);
--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -1,37 +1,28 @@
-// [START initialize_firebase_in_sw]
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.344034493973
-// [START initialize_firebase_in_sw]
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/7.6.2//firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.6.2//firebase-messaging.js');
-// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
-firebase.initializeApp({
-  apiKey: "AIzaSyBgkDA5Hv14YNUzVnWiYASMnVtpPNWxk6o",
-  authDomain: "taufikakbarmaliki-bba49.firebaseapp.com",
-  databaseURL: "https://taufikakbarmaliki-bba49.firebaseio.com",
-  projectId: "taufikakbarmaliki-bba49",
-  storageBucket: "taufikakbarmaliki-bba49.appspot.com",
-  messagingSenderId: "58729208325",
-  appId: "1:58729208325:web:870df25351a9d6cad0da6c",
-  measurementId: "G-MNR3F0DD4K"
-});
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
-const messaging = firebase.messaging();
-// [END initialize_firebase_in_sw]
-  messaging.setBackgroundMessageHandler(payload => {
-    console.log(payload)
-    // const notification = JSON.parse(payload.data.notification);
-    const notificationTitle = payload.data.title;
-    const notificationOptions = {
-      body: payload.data.body
+
+importScripts(
+  'https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js'
+)
+importScripts(
+  'https://www.gstatic.com/firebasejs/8.2.5/firebase-messaging.js'
+)
+firebase.initializeApp({"apiKey":"AIzaSyAbFVto_BzFMlbLeIwFJFWmhD-zxboOYxQ","authDomain":"kominfobalangan-d2008.firebaseapp.com","projectId":"kominfobalangan-d2008","storageBucket":"kominfobalangan-d2008.appspot.com","messagingSenderId":"494678481790","appId":"1:494678481790:web:b608138489f541ce00f12f","measurementId":"G-LJMRRD9RKK"})
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging()
+
+self.addEventListener('push', function (e) {
+    data = e.data.json();
+    var options = {
+    body: data.notification.body,
+    icon: data.notification.icon,
+    vibrate: [100, 50, 100],
+    data: {
+    dateOfArrival: Date.now(),
+    primaryKey: '2'
+    },
     };
-    //Show the notification :)
-    return self.registration.showNotification(
-      notificationTitle,
-      notificationOptions
+    e.waitUntil(
+    self.registration.showNotification(data.notification.title, options)
     );
-  });
+    });
