abb368fbd0138bb8ef3522ad9abe509e492c016d
AkbarMaliki <taufikakbarmalikitkj@gmail.com>
2021-12-05 09:26:31 +0800

    baru

--- docs/_nuxt/488b8e8.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[76],{406:function(n,w,o){"use strict";o.r(w);o(237)}}]);
--- docs/_nuxt/7b4365f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[77],{405:function(n,w,o){"use strict";o.r(w);o(236)}}]);
